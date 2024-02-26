32ad81ef904ecd82af24cf1415e3c03790eb6994
Tanisha <tanishakar04@gmail.com>
2024-02-26 20:56:46 +0530

    some minor changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./components/signin/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </BrowserRouter>
   );
