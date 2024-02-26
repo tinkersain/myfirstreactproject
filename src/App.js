c9b2c351a4b4f12f90418dd630b37f290af484f1
Tanisha <tanishakar04@gmail.com>
2024-02-26 20:45:53 +0530

    second commit changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
